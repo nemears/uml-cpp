293a359e21599a75e37a728fb06889b860d73be2
nemears <nickmears2@gmail.com>
2020-11-03 13:04:56 -0500

    removed cached files

--- libs/google_test/gtest_build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/Projects/google_test/google_test_source/googletest/googlemock
-# Build directory: /home/nick/Projects/google_test/gtest_build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- libs/google_test/gtest_build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/Projects/google_test/google_test_source/googletest/googletest
-# Build directory: /home/nick/Projects/google_test/gtest_build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

cec14bb26f84fc934f9237e5aee5020c76a45e82
nemears <nickmears2@gmail.com>
2020-11-29 12:39:46 -0500

    finished typedElementparser test

--- src/test/cpp/parsers/typedElementParserTest.cpp
+++ src/test/cpp/parsers/typedElementParserTest.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "../../../cpp/parsers/headers/modelParser.h"
 #include "../../../cpp/parsers/headers/inputParser.h"
+#include "../../../cpp/uml/headers/typedElement.h"
 
 class TypedElementParserTest : public ::testing::Test {
     public:
@@ -36,5 +37,6 @@ class TypedElementParserTest : public ::testing::Test {
 };
 
 TEST_F(TypedElementParserTest, ParseTypeTest) {
-    EXPECT_TRUE(true);
+    EXPECT_EQ(((TypedElement*)(*pp->elements)[boost::lexical_cast<boost::uuids::uuid>("16c345b4-5ae2-41ca-a0e7-a9c386ac941d")])->getType()->uuid, boost::lexical_cast<boost::uuids::uuid>("c0ab87cc-d00b-4afb-9558-538253b442b2"));
+    EXPECT_EQ(((TypedElement*)(*pp->elements)[boost::lexical_cast<boost::uuids::uuid>("190d1cb9-13dc-44e6-a064-126891ae0033")])->getType()->uuid, boost::lexical_cast<boost::uuids::uuid>("7d18ee42-82c6-4f52-8ec4-fab67a75ff35"));
 }

3b3fc68f2c0214f7972eefe49abf44b0a8c64d4e
nemears <nickmears2@gmail.com>
2020-12-06 12:59:56 -0500

    added default value to property class

--- src/cpp/uml/headers/property.h
+++ src/cpp/uml/headers/property.h
@@ -1,9 +1,11 @@
 #ifndef PROPERTY
 #define PROPERTY
 #include "typedElement.h"
+#include "valueSpecification.h"
 using namespace std;
 
 class Property: public TypedElement {
-
+    public:
+        ValueSpecification * defaultValue;
 };
 #endif

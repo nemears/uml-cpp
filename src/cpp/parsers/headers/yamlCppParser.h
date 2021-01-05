ef0b6779e1fba52bf51f86221472dca93a52b891
nemears <nickmears2@gmail.com>
2021-01-04 23:58:58 -0500

    fixed cmake linking of yaml-cpp library

--- src/cpp/parsers/headers/yamlCppParser.h
+++ src/cpp/parsers/headers/yamlCppParser.h
@@ -1,9 +1,14 @@
 #ifndef YAMLCPPPARSER
 #define YAMLCPPPARSER
 #include "inputParser.h"
+#include "yaml-cpp/yaml.h"
 
 class YamlCppParser : public InputParser {
-
+    public:
+        YamlCppParser(string f) : InputParser(f) {
+            ifstream fin(f);
+            YAML::Parser parser(fin);
+        }
 };
 
 #endif

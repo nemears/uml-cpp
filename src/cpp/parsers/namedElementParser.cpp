7d115c0d745ee17daa66f6dfa28104230877ccee
nemears <nickmears2@gmail.com>
2021-01-10 01:49:40 -0500

    got rid of old inputParser methods

--- src/cpp/parsers/namedElementParser.cpp
+++ src/cpp/parsers/namedElementParser.cpp
@@ -1,28 +1,5 @@
 #include "headers/namedElementParser.h"
 
-bool NamedElementParser::parseTag(InputParser* input, Element* el) {
-    if (input->firstWord.compare("name") == 0) {
-
-        // Check if we are overiding
-        if (((NamedElement*)el)->getName().compare("") != 0) {
-            cout << "[Error] " << keyword << " " << ((NamedElement*)el)->getName() << " duplicate name found";
-            cout << " line number " << input->lineNumber << '\n';
-            return false;
-        }
-
-        try {
-            ((NamedElement*)el)->setName(input->getTag());
-            cout << "[Info] " << keyword << " name: " << ((NamedElement*)el)->getName() << "\n";
-            return true;
-        } catch(exception& e) {
-                cerr << e.what() << '\n';
-        }
-        return true;
-    }
-
-    return ElementParser::parseTag(input, el);
-}
-
 bool NamedElementParser::parseFeatures(YAML::Node node, Element* el) {
 
     if (node["name"]) {

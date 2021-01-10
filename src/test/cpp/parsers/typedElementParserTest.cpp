5a5226c0c675615bf1f105a83a7ba48f4bfe80d2
nemears <nickmears2@gmail.com>
2021-01-10 01:39:20 -0500

    added typedElementParser test with yaml-cpp

--- src/test/cpp/parsers/typedElementParserTest.cpp
+++ src/test/cpp/parsers/typedElementParserTest.cpp
@@ -5,8 +5,9 @@
 
 class TypedElementParserTest : public ::testing::Test {
     public:
-        ModelParser* pp;
+        ModelParser* pp, *ppYAML;
         InputParser* input;
+        YAML::Node node;
     protected:
   // You can remove any or all of the following functions if their bodies would
   // be empty.
@@ -15,6 +16,9 @@ class TypedElementParserTest : public ::testing::Test {
     pp = new ModelParser(new map<boost::uuids::uuid, Element*>);
     input = new InputParser("../../../../../src/test/yml/typedElementTests/typedElement.yml"); // root file is the gmock_main which is like 7 down right now
     // TODO fix google_test within directory structure
+
+    ppYAML = new ModelParser(new map<boost::uuids::uuid, Element*>);
+    node = YAML::LoadFile("../../../../../src/test/yml/typedElementTests/typedElement.yml");
   }
 
   ~TypedElementParserTest() override {
@@ -24,6 +28,7 @@ class TypedElementParserTest : public ::testing::Test {
   void SetUp() override {
     input->readNextLine();
     pp->parse(input);
+    ppYAML->parse(node);
   }
 
   void TearDown() override {
@@ -39,4 +44,8 @@ class TypedElementParserTest : public ::testing::Test {
 TEST_F(TypedElementParserTest, ParseTypeTest) {
     EXPECT_EQ(((TypedElement*)(*pp->elements)[boost::lexical_cast<boost::uuids::uuid>("16c345b4-5ae2-41ca-a0e7-a9c386ac941d")])->getType()->uuid, boost::lexical_cast<boost::uuids::uuid>("c0ab87cc-d00b-4afb-9558-538253b442b2"));
     EXPECT_EQ(((TypedElement*)(*pp->elements)[boost::lexical_cast<boost::uuids::uuid>("190d1cb9-13dc-44e6-a064-126891ae0033")])->getType()->uuid, boost::lexical_cast<boost::uuids::uuid>("7d18ee42-82c6-4f52-8ec4-fab67a75ff35"));
+
+    EXPECT_EQ(((TypedElement*)(*ppYAML->elements)[boost::lexical_cast<boost::uuids::uuid>("16c345b4-5ae2-41ca-a0e7-a9c386ac941d")])->getType()->uuid, boost::lexical_cast<boost::uuids::uuid>("c0ab87cc-d00b-4afb-9558-538253b442b2"));
+    EXPECT_EQ(((TypedElement*)(*ppYAML->elements)[boost::lexical_cast<boost::uuids::uuid>("190d1cb9-13dc-44e6-a064-126891ae0033")])->getType()->uuid, boost::lexical_cast<boost::uuids::uuid>("7d18ee42-82c6-4f52-8ec4-fab67a75ff35"));
+
 }

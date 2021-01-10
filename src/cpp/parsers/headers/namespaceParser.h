2fd78862755fb54edd5d2a9a28f20808c7228238
nemears <nickmears2@gmail.com>
2021-01-10 01:50:11 -0500

    got rid of old inputParser methods

--- src/cpp/parsers/headers/namespaceParser.h
+++ src/cpp/parsers/headers/namespaceParser.h
@@ -6,8 +6,6 @@
 
 class NamespaceParser : public NamedElementParser {
     protected:
-        bool parseTag(InputParser* inputParser, Element * el);
-
         virtual bool parseFeatures(YAML::Node node, Element* el);
     public:
         NamespaceParser(map<boost::uuids::uuid, Element*>* elements) : NamedElementParser(elements){
--- src/cpp/parsers/namespaceParser.cpp
+++ src/cpp/parsers/namespaceParser.cpp
@@ -1,9 +1,5 @@
 #include "headers/namespaceParser.h"
 
-bool NamespaceParser::parseTag(InputParser* input, Element* el) {
-    return NamedElementParser::parseTag(input, el);
-}
-
 bool NamespaceParser::parseFeatures(YAML::Node node, Element* el) {
     return NamedElementParser::parseFeatures(node, el);
 }

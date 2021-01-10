bf3a28bf771012c063ee7c0c9b9b0c46fd706163
nemears <nickmears2@gmail.com>
2021-01-10 01:47:58 -0500

    got rid of old inputParser methods in templateParser

--- src/cpp/parsers/headers/templateParser.h
+++ src/cpp/parsers/headers/templateParser.h
@@ -12,30 +12,18 @@ using namespace std;
 
 class TemplateParser {
     protected:
-        virtual bool parseTag(InputParser* inputParser, Element* el) = 0;
         virtual Element* createElement() = 0; // interface
-
-        // new function interface for yaml-cpp parser to get details of uml element type from file
         virtual bool parseFeatures(YAML::Node node, Element* el) = 0;
 
     public:
         string keyword;
         map<boost::uuids::uuid, Element*>* elements;
-
-        // old top level parse function
-        bool parse(InputParser* inputParser);
-
-        // new top level parse function
         bool parse(YAML::Node node);
 
         virtual ~TemplateParser() {
             //delete elements;
         };
-
-        // Old function using inputParser parser
-        Element* parseElement(InputParser* inputParser);
-
-        // New function using yaml-cpp parser
+        
         Element* parseElement(YAML::Node node);
         
         /**
--- src/cpp/parsers/templateParser.cpp
+++ src/cpp/parsers/templateParser.cpp
@@ -1,27 +1,5 @@
 #include "headers/templateParser.h"
 
-bool TemplateParser::parse(InputParser* input) {
-    cout << "[Info] keyword " << keyword << '\n';
-    int startTabs = input->numTabs;
-    while (input->readNextLine()) {
-        // identify comments 
-        cout << "[Info] first word " << input->firstWord << '\n';
-        if (input->shouldSkip()) {
-            cout << "skipping\n";
-            continue;
-        // parse element
-        } else if (input->firstWord.compare(keyword) == 0) {
-            parseElement(input);
-            return true;
-        // it ended
-        } else if (input->numTabs <= startTabs) {
-            cerr << "[Error] didn't parse " << keyword << " \n";
-            break;
-        }
-    }
-    return false;
-}
-
 bool TemplateParser::parse(YAML::Node node) {
     cout << "[Info] keyword " << keyword << '\n';
     if (node[keyword]) {
@@ -30,32 +8,6 @@ bool TemplateParser::parse(YAML::Node node) {
     }
 
     return false;
-}
-
-Element *TemplateParser::parseElement(InputParser* input) {
-    int numTabs = input->numTabs;
-    Element * el = createElement();
-    (*elements)[el->uuid] = el;
-    while(input->readNextLine()) {
-        // check if line is valid
-        if (input->shouldSkip()) {
-            continue;
-        //} else if (input->numTabs <= numTabs) {
-            // cout << "[Info] Finished parsing " << keyword << " " << el->getID() << '\n';
-            // break;
-        // check if value is readable
-        } else if (parseTag(input, el)) {
-            
-        } else {
-            // TODO log error
-            cerr << "[Error] Could not recognize tag " << input->firstWord << " line " << input->lineNumber << "\n";
-        }
-        if (input->nextLineTabs <= numTabs) {
-            break;
-        }
-    }
-    cout << "\n[Info] created " << keyword << " " << el->uuid << '\n';
-    return el;
 }
 
 Element* TemplateParser::parseElement(YAML::Node node) {

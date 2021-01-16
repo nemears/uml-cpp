f2309401bef0943d56a65451cc940ccdaa2282f0
nemears <nickmears2@gmail.com>
2021-01-16 05:02:38 -0500

    added default value to parsing

--- src/cpp/parsers/headers/propertyParser.h
+++ src/cpp/parsers/headers/propertyParser.h
@@ -2,6 +2,11 @@
 #define PROPERTYPARSER
 #include "typedElementParser.h"
 #include "../../uml/headers/property.h"
+#include "../../uml/headers/primitiveType.h"
+#include "../../uml/headers/literalString.h"
+#include "../../uml/headers/literalInt.h"
+#include "../../uml/headers/literalReal.h"
+#include "../../uml/headers/literalBool.h"
 using namespace std;
 
 class PropertyParser : public TypedElementParser {
--- src/cpp/parsers/propertyParser.cpp
+++ src/cpp/parsers/propertyParser.cpp
@@ -1,13 +1,44 @@
 #include "headers/propertyParser.h"
 
-UML::Element* PropertyParser::createElement(){
-    return new UML::Property;
+using namespace UML;
+
+Element* PropertyParser::createElement(){
+    return new Property;
 }
 
 bool PropertyParser::parseFeatures(YAML::Node node, UML::Element* el) {
+
+    bool ret = TypedElementParser::parseFeatures(node, el);
+
     if (node["defaultValue"]) {
-        // TODO
+
+        if (((Property*) el)->getType()->isPrimitive()) {
+            switch (((PrimitiveType*) ((Property*)el)->getType())->getPrimitiveType()) {
+                case PrimitiveType::Primitive::STRING : {
+                    string stringVal = node["defaultValue"].as<string>();
+                    LiteralString ls;
+                    ls.setValue(stringVal);
+                }
+                case PrimitiveType::Primitive::INT : {
+                    int intVal = node["defaultValue"].as<int>();
+                    LiteralInt li;
+                    li.setValue(intVal);
+                }
+                case PrimitiveType::Primitive::REAL : {
+                    double realVal = node["defaultValue"].as<double>();
+                    LiteralReal lr;
+                    lr.setValue(realVal);
+                }
+                case PrimitiveType::Primitive::BOOL : {
+                    bool boolBal = node["defaultValue"].as<bool>();
+                    LiteralBool lb;
+                    lb.setValue(boolBal);
+                }
+            }
+        } else {
+            // instances
+        }
     }
 
-    return TypedElementParser::parseFeatures(node, el);
+    return ret;
 }

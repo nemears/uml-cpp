74d116e9aaffc7a3e2ab670fc0c68386a587af62
nemears <nickmears2@gmail.com>
2021-01-23 04:45:26 -0500

    added beggining of parsing for operations

--- src/cpp/parsers/operationParser.cpp
+++ src/cpp/parsers/operationParser.cpp
@@ -6,5 +6,21 @@ Element* OperationParser::createElement() {
 
 bool OperationParser::parseFeatures(YAML::Node node, Element* el) {
 
+    if (node["parameters"]) {
+        if (node["parameters"].IsSequence()) {
+            for (std::size_t i=0; i<node["parameters"].size(); i++) {
+                // TODO parameterParser
+            }
+        }
+    }
+
+    if (node["methods"]) {
+        if (node["methods"].IsSequence()) {
+            for (std::size_t i=0; i<node["methods"].size(); i++) {
+                // TODO opaqueBehaviorParser
+            }
+        }
+    }
+
     return NamedElementParser::parseFeatures(node, el);
 }

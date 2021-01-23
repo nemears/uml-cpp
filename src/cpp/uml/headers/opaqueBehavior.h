532ccd8b702f706e6a5743c7686a7f087854dd12
nemears <nickmears2@gmail.com>
2021-01-23 03:59:31 -0500

    added bodies to opaque behaviors

--- src/cpp/uml/headers/opaqueBehavior.h
+++ src/cpp/uml/headers/opaqueBehavior.h
@@ -1,11 +1,14 @@
 #ifndef OPAQUEBEHAVIOR
 #define OPAQUEBEHAVIOR
 
+#include <list>
+#include <string>
 #include "behavior.h"
 
 namespace UML {
     class OpaqueBehavior : public Behavior {
-
+        public:
+            list<string> bodies;
     };
 }
 

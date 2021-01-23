e6a1d9fc97aa4967f34fcb88de6cf04cd6ca40cf
nemears <nickmears2@gmail.com>
2021-01-23 03:55:23 -0500

    implemented list of parameters

--- src/cpp/uml/headers/operation.h
+++ src/cpp/uml/headers/operation.h
@@ -4,11 +4,14 @@
 #include <list>
 #include "namedElement.h"
 #include "behavior.h"
+#include "parameter.h"
+
 using namespace std;
 namespace UML {
     class Operation : public NamedElement {
         public:
             list<Behavior*> methods;
+            list<Parameter*> parameters;
     };
 }
 

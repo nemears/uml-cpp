a3ed624e76425307c14a716381ae44df60e088ac
nemears <nickmears2@gmail.com>
2021-02-06 01:48:55 -0500

    fixed typo in test config file and finished test

--- src/test/cpp/yuml-parsers/parameterParserTest.cpp
+++ src/test/cpp/yuml-parsers/parameterParserTest.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "yuml-parsers/modelParser.h"
+#include "uml/parameter.h"
 
 using namespace UML;
 
@@ -8,7 +9,11 @@ class ParameterParserTest : public ::testing::Test {
 };
 
 TEST_F(ParameterParserTest, TestParseDirection) {
+    // Setup
     ModelParser directionParser(new map<boost::uuids::uuid, Element*>);
     YAML::Node directionNode = YAML::LoadFile("../../../../../src/test/yml/parameterTests/parameterDirection.yml");
+
+    // Test
     ASSERT_NO_THROW(directionParser.parse(directionNode));
+    ASSERT_TRUE(((Parameter*)(*directionParser.elements)[boost::lexical_cast<boost::uuids::uuid>("190d1cb9-13dc-44e6-a064-126891ae0033")])->getDirection() == ParameterDirectionKind::IN);
 }

1f4459024d72e022d7c6127d26cc4cbd4885d93b
nemears <nickmears2@gmail.com>
2021-02-06 02:03:32 -0500

    added emit test for parser

--- src/test/cpp/yuml-parsers/parameterParserTest.cpp
+++ src/test/cpp/yuml-parsers/parameterParserTest.cpp
@@ -17,3 +17,29 @@ TEST_F(ParameterParserTest, TestParseDirection) {
     ASSERT_NO_THROW(directionParser.parse(directionNode));
     ASSERT_TRUE(((Parameter*)(*directionParser.elements)[boost::lexical_cast<boost::uuids::uuid>("190d1cb9-13dc-44e6-a064-126891ae0033")])->getDirection() == ParameterDirectionKind::IN);
 }
+
+TEST_F(ParameterParserTest, TestEmitDirection) {
+    // Setup
+    Model m;
+    m.setID("190d1cb9-13dc-44e6-a064-126891ae0033");
+    Parameter p;
+    p.setID("16c345b4-5ae2-41ca-a0e7-a9c386ac941d");
+    p.setDirection(ParameterDirectionKind::RETURN);
+    m.ownedElements.push_back(&p);
+
+    string expectedEmit = R""""(model:
+  id: 190d1cb9-13dc-44e6-a064-126891ae0033
+  children:
+      - parameter:
+          id: 16c345b4-5ae2-41ca-a0e7-a9c386ac941d
+          direction: IN)"""";
+
+    ModelParser mp(new map<boost::uuids::uuid, Element*>);
+    string generatedEmit;
+    YAML::Emitter emitter;
+    ASSERT_NO_THROW(mp.emit(emitter, &m));
+    generatedEmit = emitter.c_str();
+    cout << generatedEmit << '\n';
+    ASSERT_TRUE(emitter.good());
+    ASSERT_EQ(expectedEmit, generatedEmit);
+}

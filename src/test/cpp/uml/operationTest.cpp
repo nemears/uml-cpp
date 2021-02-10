5acda397aa33f77f4ceaa4727389e5c05684bd70
nemears <nickmears2@gmail.com>
2021-02-09 22:37:41 -0500

    added cpp test for operations

--- src/test/cpp/uml/operationTest.cpp
+++ src/test/cpp/uml/operationTest.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "uml/operation.h"
+#include "uml/opaqueBehavior.h"
 
 using namespace UML;
 
@@ -21,4 +22,22 @@ TEST_F(OperationTest, RemoveParameterTest) {
     ASSERT_TRUE(o.parameters.front()->uuid == p.uuid);
     ASSERT_NO_THROW(o.removeParameter(p));
     ASSERT_TRUE(o.parameters.front()->uuid == p2.uuid);
+}
+
+TEST_F(OperationTest, AddMethodTest) {
+    Operation o;
+    OpaqueBehavior b;
+    ASSERT_NO_THROW(o.addMethod(b));
+    ASSERT_TRUE(o.methods.front()->uuid == b.uuid);
+}
+
+TEST_F(OperationTest, RemoveMethodTest) {
+    Operation o;
+    OpaqueBehavior b;
+    OpaqueBehavior b2;
+    o.addMethod(b);
+    o.addMethod(b2);
+    ASSERT_TRUE(o.methods.front()->uuid == b.uuid);
+    ASSERT_NO_THROW(o.removeMethod(b));
+    ASSERT_TRUE(o.methods.front()->uuid == b2.uuid);
 }

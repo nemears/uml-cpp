1d82bb69a5c5db8dc5a906907e57479def8f4672
nemears <nickmears2@gmail.com>
2021-02-13 15:17:42 -0500

    added objectNode

---
+++ include/uml/objectNode.h
@@ -0,0 +1,13 @@
+#ifndef OBJECTNODE_H
+#define OBJECTNODE_H
+
+#include "activityNode.h"
+#include "typedElement.h"
+
+namespace UML {
+    class ObjectNode : public ActivityNode, public TypedElement {
+
+    };
+}
+
+#endif
---
+++ src/cpp/uml/objectNode.cpp
@@ -0,0 +1,2 @@
+#include "uml/objectNode.h"
+

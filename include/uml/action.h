8dd50fafc2a93d7133c50ba8dc0b2017b9961800
nemears <nickmears2@gmail.com>
2021-02-13 17:27:23 -0500

    added input and output to actions

--- include/uml/action.h
+++ include/uml/action.h
@@ -2,10 +2,14 @@
 #define ACTION_H
 
 #include "activityNode.h"
+#include "inputPin.h"
+#include "outputPin.h"
 
 namespace UML {
     class Action : public ActivityNode {
         public:
+            list<InputPin*> inputs;
+            list<OutputPin*> outputs;
             ElementType getElementType() override;
     };
 }

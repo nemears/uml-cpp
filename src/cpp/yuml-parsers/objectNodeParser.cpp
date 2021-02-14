cfcdcef1d439ddc2e60005c6bbd24b834e89bcfc
nemears <nickmears2@gmail.com>
2021-02-14 18:14:39 -0500

    added element defenition in emit

--- src/cpp/yuml-parsers/objectNodeParser.cpp
+++ src/cpp/yuml-parsers/objectNodeParser.cpp
@@ -13,10 +13,21 @@ bool ObjectNodeParser::parseFeatures(YAML::Node node, Element* el) {
 
 bool ObjectNodeParser::emit(YAML::Emitter& emitter, Element* el) {
 
+    if (el->getElementType() == ElementType::OBJECT_NODE) {
+        emitter << YAML::BeginMap;
+        emitter << YAML::Key << "objectNode";
+        emitter << YAML::BeginMap;
+    }
+
     bool ret = emitType(emitter, el);
 
     if (!emitActivityNode(emitter, el)) {
         ret = false;
+    }
+
+    if (el->getElementType() == ElementType::OBJECT_NODE) {
+        emitter << YAML::EndMap;
+        emitter << YAML::EndMap;
     }
 
     return ret;

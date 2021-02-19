a2c001d5c59d62358f9f9b1a4359c5243cc36f2f
nemears <nickmears2@gmail.com>
2021-02-19 01:15:39 -0500

    expanded it

--- src/test/cpp/yuml-parsers/controlFlowParserTest.cpp
+++ src/test/cpp/yuml-parsers/controlFlowParserTest.cpp
@@ -37,7 +37,8 @@ TEST_F(ControlFlowParserTest, ActionToActionFlowTest) {
     // Test second action
     ASSERT_TRUE(dynamic_cast<Activity*>(actionToActionFlowParser.theEl->ownedElements.front())->nodes.back()->incoming.size() == 1);
     ASSERT_TRUE(dynamic_cast<Activity*>(actionToActionFlowParser.theEl->ownedElements.front())->nodes.back()->incoming.front() != 0);
-
+    ASSERT_EQ(dynamic_cast<Activity*>(actionToActionFlowParser.theEl->ownedElements.front())->uuid, dynamic_cast<Activity*>(actionToActionFlowParser.theEl->ownedElements.front())->nodes.back()->getActivity()->uuid);
+    
     // Test control flow
     ASSERT_TRUE(dynamic_cast<Activity*>(actionToActionFlowParser.theEl->ownedElements.front())->nodes.back()->incoming.front()->getSource()->uuid == boost::lexical_cast<boost::uuids::uuid>("2c2fc607-48c6-4879-800b-7e659441cba1"));
     ASSERT_TRUE(dynamic_cast<Activity*>(actionToActionFlowParser.theEl->ownedElements.front())->nodes.back()->incoming.front()->getTarget()->uuid == boost::lexical_cast<boost::uuids::uuid>("7d4b6b0b-f6c2-4670-868c-87709cede18e"));

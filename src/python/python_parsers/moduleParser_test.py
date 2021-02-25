f0129d077fdc3418a3834f9919019ab07b28a27f
nemears <nickmears2@gmail.com>
2021-02-25 00:20:01 -0500

    added test for function

--- src/python/python_parsers/moduleParser_test.py
+++ src/python/python_parsers/moduleParser_test.py
@@ -1,6 +1,6 @@
 import unittest
 from moduleParser import parseModule2
-from yuml_python import Namespace, Class
+from yuml_python import Namespace, Class, Activity
 
 class parseModuleTest(unittest.TestCase):
 
@@ -10,6 +10,14 @@ class parseModuleTest(unittest.TestCase):
         self.assertEqual(type(m), Namespace)
         self.assertEqual(len(m.ownedElements), 1)
         self.assertEqual(type(m.ownedElements[0]), Class)
+
+    def testParseFunc(self):
+        d = {}
+        m = parseModule2('/home/stinky/Projects/yuml_projects/yuml/src/test/python/examples/fun.py', d)
+        self.assertEqual(type(m), Namespace)
+        self.assertEqual(len(m.ownedElements), 2)
+        self.assertEqual(type(m.ownedElements[0]), Activity)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ src/test/python/examples/fun.py
@@ -0,0 +1,6 @@
+def noParam():
+    return true
+
+def numParam(f):
+    f += 1
+    return f

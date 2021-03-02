8dfd9997160396e3e66d800babd57540a62d944a
nemears <nickmears2@gmail.com>
2021-03-01 23:47:42 -0500

    added upperBound parsing for object nodes

--- src/cpp/yuml-parsers/objectNodeParser.cpp
+++ src/cpp/yuml-parsers/objectNodeParser.cpp
@@ -23,6 +23,71 @@ bool ObjectNodeParser::parseObjectNodeFeatures(YAML::Node node, Element* el) {
         ret = false;
     }
 
+    if (node["upperBound"]) {
+        if (dynamic_cast<ObjectNode*>(el)->getType() != NULL) {
+            if (dynamic_cast<ObjectNode*>(el)->getType()->isPrimitive()) {
+                switch (((PrimitiveType*) dynamic_cast<ObjectNode*>(el)->getType())->getPrimitiveType()) {
+                    case PrimitiveType::Primitive::STRING : {
+                        string stringVal = node["upperBound"].as<string>();
+                        LiteralString* ls = new LiteralString;
+                        ls->setValue(stringVal);
+                        dynamic_cast<ObjectNode*>(el)->setUpperBound(ls);
+                        break;
+                    }
+                    case PrimitiveType::Primitive::INT : {
+                        int intVal = node["upperBound"].as<int>();
+                        LiteralInt* li = new LiteralInt;
+                        li->setValue(intVal);
+                        dynamic_cast<ObjectNode*>(el)->setUpperBound(li);
+                        break;
+                    }
+                    case PrimitiveType::Primitive::REAL : {
+                        double realVal = node["upperBound"].as<double>();
+                        LiteralReal* lr = new LiteralReal;
+                        lr->setValue(realVal);
+                        dynamic_cast<ObjectNode*>(el)->setUpperBound(lr);
+                        break;
+                    }
+                    case PrimitiveType::Primitive::BOOL : {
+                        bool boolVal = node["upperBound"].as<bool>();
+                        LiteralBool* lb = new LiteralBool;
+                        lb->setValue(boolVal);
+                        dynamic_cast<ObjectNode*>(el)->setUpperBound(lb);
+                        break;
+                    }
+                    default : {
+                        //TODO error
+                        break;
+                    }
+                }
+            } else {
+                if (node["upperBound"].IsScalar()) {
+                    // instances
+                    string parsedId = node["upperBound"].as<string>();
+                    if (isValidUUID4(parsedId)) {
+                        boost::uuids::uuid typeId = boost::lexical_cast<boost::uuids::uuid>(parsedId);
+                        InstanceSpecification* defaultVal = dynamic_cast<InstanceSpecification*>((*TypedElementParser::elements)[typeId]);
+                        InstanceValue* instanceVal = new InstanceValue;
+                        instanceVal->setInstance(defaultVal);
+                        dynamic_cast<ObjectNode*>(el)->setUpperBound(instanceVal);
+                    }
+                } else if (node["upperBound"].IsMap()) {
+                    if (node["upperBound"]["expression"]) {
+                        ExpressionParser ep(TypedElementParser::elements, TypedElementParser::postProcessFlag);
+                        Element* parsedEl = ep.parseElement(node["upperBound"]["expression"]);
+                        dynamic_cast<ObjectNode*>(el)->setUpperBound(dynamic_cast<ValueSpecification*>(parsedEl));
+                    }
+                } else {
+                    // TODO throw error
+                    ret = false
+                }
+            }
+        } else {
+            // TODO throw error
+            ret = false;
+        }
+    }
+
     return ret;
 }
 

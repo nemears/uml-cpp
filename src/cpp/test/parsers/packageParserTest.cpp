61ebe04c6fbac672703c14dee8c6a4dfdbae4846
nemears <nickmears2@gmail.com>
2021-04-09 17:08:02 -0400

    added a little more in depth test

--- src/cpp/test/parsers/packageParserTest.cpp
+++ src/cpp/test/parsers/packageParserTest.cpp
@@ -30,3 +30,39 @@ TEST_F(PackageParserTest, parsePackageWithActivityTest) {
     ASSERT_TRUE(act->getNamespace() == pckg);
     ASSERT_TRUE(act->getOwner() == pckg);
 }
+
+TEST_F(PackageParserTest, parse3PackagesTest) {
+    Element* el = Parsers::parse(YAML::LoadFile(ymlPath + "packageParserTests/3packages.yml"));
+    ASSERT_TRUE(el);
+    ASSERT_TRUE(el->getElementType() == ElementType::PACKAGE);
+    Package* pckg1 = dynamic_cast<Package*>(el);
+    ASSERT_TRUE(pckg1->getPackagedElements().size() == 2);
+    ASSERT_TRUE(pckg1->getPackagedElements().front()->getElementType() == ElementType::PACKAGE);
+    ASSERT_TRUE(pckg1->getPackagedElements().back()->getElementType() == ElementType::PACKAGE);
+    Package* pckg2 = dynamic_cast<Package*>(pckg1->getPackagedElements().front());
+    Package* pckg3 = dynamic_cast<Package*>(pckg1->getPackagedElements().back());
+    ASSERT_TRUE(pckg2->getOwningPackage() == pckg1);
+    ASSERT_TRUE(pckg3->getOwningPackage() == pckg1);
+    ASSERT_TRUE(pckg1->getMembers().size() == 2);
+    ASSERT_TRUE(pckg1->getMembers().front() == pckg2);
+    ASSERT_TRUE(pckg1->getMembers().back() == pckg3);
+    ASSERT_TRUE(pckg2->getNamespace() == pckg1);
+    ASSERT_TRUE(pckg3->getNamespace() == pckg1);
+    ASSERT_TRUE(pckg1->getOwnedElements().size() == 2);
+    ASSERT_TRUE(pckg1->getOwnedElements().front() == pckg2);
+    ASSERT_TRUE(pckg1->getOwnedElements().back() == pckg3);
+    ASSERT_TRUE(pckg2->getOwner() == pckg1);
+    ASSERT_TRUE(pckg3->getOwner() == pckg1);
+    ASSERT_TRUE(pckg2->getPackagedElements().size() == 1);
+    ASSERT_TRUE(pckg2->getPackagedElements().front()->getElementType() == ElementType::ACTIVITY);
+    Activity* act1 = dynamic_cast<Activity*>(pckg2->getPackagedElements().front());
+    ASSERT_TRUE(act1->getOwningPackage() == pckg2);
+    ASSERT_TRUE(act1->getNamespace() == pckg2);
+    ASSERT_TRUE(act1->getOwner() == pckg2);
+    ASSERT_TRUE(pckg3->getPackagedElements().size() == 1);
+    ASSERT_TRUE(pckg3->getPackagedElements().front()->getElementType() == ElementType::ACTIVITY);
+    Activity* act2 = dynamic_cast<Activity*>(pckg3->getPackagedElements().front());
+    ASSERT_TRUE(act2->getOwningPackage() == pckg3);
+    ASSERT_TRUE(act2->getNamespace() == pckg3);
+    ASSERT_TRUE(act2->getOwner() == pckg3);
+}

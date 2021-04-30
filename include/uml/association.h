571baf81b1448920074b6ebf4bc367ea5a010b58
nemears <nickmears2@gmail.com>
2021-04-29 23:06:56 -0400

    added ends to association

--- include/uml/association.h
+++ include/uml/association.h
@@ -7,7 +7,15 @@
 namespace UML {
     class Association : public Classifier, public Relationship {
         protected:
+            Sequence<Property>* m_memberEnds;
+            Sequence<Property>* m_ownedEnds;
+            Sequence<Property>* m_navigableOwnedEnds;
         public:
+            Association();
+            ~Association();
+            Sequence<Property>& getMemberEnds();
+            Sequence<Property>& getOwnedEnds();
+            Sequence<Property>& getNavigableOwnedEnds();
             ElementType getElementType() override;
             bool isSubClassOf(ElementType eType);
     };
--- src/cpp/uml/association.cpp
+++ src/cpp/uml/association.cpp
@@ -2,6 +2,30 @@
 
 using namespace UML;
 
+Association::Association() {
+    m_memberEnds = new Sequence<Property>;
+    m_ownedEnds = new Sequence<Property>;
+    m_navigableOwnedEnds = new Sequence<Property>;
+}
+
+Association::~Association() {
+    delete m_memberEnds;
+    delete m_ownedEnds;
+    delete m_navigableOwnedEnds;
+}
+
+Sequence<Property>& Association::getMemberEnds() {
+    return *m_memberEnds;
+}
+
+Sequence<Property>& Association::getOwnedEnds() {
+    return *m_ownedEnds;
+}
+
+Sequence<Property>& Association::getNavigableOwnedEnds() {
+    return *m_navigableOwnedEnds;
+}
+
 ElementType Association::getElementType() {
     return ElementType::ASSOCIATION;
 }

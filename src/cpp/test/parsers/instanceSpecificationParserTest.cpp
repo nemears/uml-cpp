b812c64ed892da74aa76874dbb29e34abb9c87b3
nemears <nickmears2@gmail.com>
2021-05-04 23:24:59 -0400

    added slot tests

--- src/cpp/test/parsers/instanceSpecificationParserTest.cpp
+++ src/cpp/test/parsers/instanceSpecificationParserTest.cpp
@@ -43,3 +43,41 @@ TEST_F(InstanceSpecificationParserTest, backwardsClassifierTest) {
 }
 
 // TODO slot tests
+
+TEST_F(InstanceSpecificationParserTest, basicSlotTest) {
+    Element* el;
+    ASSERT_NO_THROW(el = Parsers::parse(YAML::LoadFile(ymlPath + "instanceSpecificationTests/basicSlot.yml")));
+    ASSERT_TRUE(el->getElementType() == ElementType::PACKAGE);
+    Package* pckg = dynamic_cast<Package*>(el);
+    ASSERT_TRUE(pckg->getPackagedElements().size() == 2);
+    ASSERT_TRUE(pckg->getPackagedElements().back()->getElementType() == ElementType::INSTANCE_SPECIFICATION);
+    ASSERT_TRUE(pckg->getPackagedElements().front()->getElementType() == ElementType::CLASS);
+    Class* c = dynamic_cast<Class*>(pckg->getPackagedElements().front());
+    InstanceSpecification i = *dynamic_cast<InstanceSpecification*>(pckg->getPackagedElements().back());
+    ASSERT_TRUE(i.getClassifier() == c);
+    ASSERT_TRUE(i.getSlots().size() == 1);
+    ASSERT_TRUE(c->getOwnedAttributes().size() == 1);
+    Slot* s = i.getSlots().front();
+    Property* p = c->getOwnedAttributes().front();
+    ASSERT_TRUE(s->getDefiningFeature() == p);
+    ASSERT_TRUE(s->getOwningInstance() == pckg->getPackagedElements().back());
+}
+
+TEST_F(InstanceSpecificationParserTest, backwardsSlotTest) {
+    Element* el;
+    ASSERT_NO_THROW(el = Parsers::parse(YAML::LoadFile(ymlPath + "instanceSpecificationTests/backwardsSlot.yml")));
+    ASSERT_TRUE(el->getElementType() == ElementType::PACKAGE);
+    Package* pckg = dynamic_cast<Package*>(el);
+    ASSERT_TRUE(pckg->getPackagedElements().size() == 2);
+    ASSERT_TRUE(pckg->getPackagedElements().front()->getElementType() == ElementType::INSTANCE_SPECIFICATION);
+    ASSERT_TRUE(pckg->getPackagedElements().back()->getElementType() == ElementType::CLASS);
+    Class* c = dynamic_cast<Class*>(pckg->getPackagedElements().back());
+    InstanceSpecification i = *dynamic_cast<InstanceSpecification*>(pckg->getPackagedElements().front());
+    ASSERT_TRUE(i.getClassifier() == c);
+    ASSERT_TRUE(i.getSlots().size() == 1);
+    ASSERT_TRUE(c->getOwnedAttributes().size() == 1);
+    Slot* s = i.getSlots().front();
+    Property* p = c->getOwnedAttributes().front();
+    ASSERT_TRUE(s->getDefiningFeature() == p);
+    ASSERT_TRUE(s->getOwningInstance() == pckg->getPackagedElements().front());
+}

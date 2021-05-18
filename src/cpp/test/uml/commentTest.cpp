2b7ebe606c7cbdc65cc4b6460b12f965a2da85df
nemears <nickmears2@gmail.com>
2021-05-18 19:58:36 -0400

    added more comment tests

--- src/cpp/test/uml/commentTest.cpp
+++ src/cpp/test/uml/commentTest.cpp
@@ -18,4 +18,26 @@ TEST_F(CommentTest, addOwnedCommentTest) {
     ASSERT_TRUE(e.getOwnedComments().front() == &c);
     ASSERT_TRUE(e.getOwnedElements().size() == 1);
     ASSERT_TRUE(e.getOwnedElements().front() == &c);
+    ASSERT_TRUE(c.getOwningElement() == &e);
+}
+
+TEST_F(CommentTest, removeOwnedCommentTest) {
+    Class e;
+    Comment c;
+    c.setBody("p cool comment");
+    e.getOwnedComments().add(c);
+    ASSERT_NO_THROW(e.getOwnedComments().remove(c));
+    ASSERT_TRUE(e.getOwnedComments().size() == 0);
+    ASSERT_TRUE(e.getOwnedElements().size() == 0);
+    ASSERT_TRUE(c.getOwningElement() == 0);
+}
+
+TEST_F(CommentTest, overideOwningElement) {
+    Class e;
+    Comment c;
+    c.setOwningElement(&e);
+    ASSERT_TRUE(e.getOwnedComments().size() == 1);
+    ASSERT_TRUE(e.getOwnedComments().front() == &c);
+    ASSERT_TRUE(e.getOwnedElements().size() == 1);
+    ASSERT_TRUE(e.getOwnedElements().front() == &c);
 }

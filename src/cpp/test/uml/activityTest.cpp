d7176a2195c137301be5594c498d3949f3ca5489
unknown <nickmears2@gmail.com>
2021-09-11 23:31:47 -0400

    modernized ActivityTest

--- src/cpp/test/uml/activityTest.cpp
+++ src/cpp/test/uml/activityTest.cpp
@@ -10,49 +10,61 @@ class ActivityTest : public ::testing::Test {
 };
 
 TEST_F(ActivityTest, addNodeFunctorTest) {
-    Activity a;
-    ActivityNode n;
+    UmlManager m;
+    Activity a = m.create<Activity>();
+    ActivityNode n = m.create<ActivityNode>();
     a.getNodes().add(n);
-    ASSERT_TRUE(a.getNodes().size() == 1);
-    ASSERT_TRUE(&a.getNodes().front() == &n);
-    ASSERT_TRUE(a.getOwnedElements().size() == 1);
-    ASSERT_TRUE(&a.getOwnedElements().front() == &n);
-    ASSERT_TRUE(n.getActivity() == &a);
-    ASSERT_TRUE(n.getOwner() == &a);
+    ASSERT_EQ(a.getNodes().size(), 1);
+    ASSERT_EQ(a.getNodes().front(), n);
+    ASSERT_EQ(a.getOwnedElements().size(), 1);
+    ASSERT_EQ(a.getOwnedElements().front(), n);
+    ASSERT_TRUE(n.hasActivity());
+    ASSERT_TRUE(n.getActivity());
+    ASSERT_EQ(n.getActivityRef(), a);
+    ASSERT_EQ(n.getOwner(), &a);
 }
 
 TEST_F(ActivityTest, setNodeActivityTest) {
-    Activity a;
-    ActivityNode n;
-    n.setActivity(&a);
-    ASSERT_TRUE(a.getNodes().size() == 1);
-    ASSERT_TRUE(&a.getNodes().front() == &n);
-    ASSERT_TRUE(a.getOwnedElements().size() == 1);
-    ASSERT_TRUE(&a.getOwnedElements().front() == &n);
-    ASSERT_TRUE(n.getActivity() == &a);
-    ASSERT_TRUE(n.getOwner() == &a);
+    UmlManager m;
+    Activity a = m.create<Activity>();
+    ActivityNode n = m.create<ActivityNode>();
+    n.setActivity(a);
+    ASSERT_EQ(a.getNodes().size(), 1);
+    ASSERT_EQ(a.getNodes().front(), n);
+    ASSERT_EQ(a.getOwnedElements().size(), 1);
+    ASSERT_EQ(a.getOwnedElements().front(), n);
+    ASSERT_TRUE(n.hasActivity());
+    ASSERT_TRUE(n.getActivity());
+    ASSERT_EQ(n.getActivityRef(), a);
+    ASSERT_EQ(n.getOwner(), &a);
 }
 
 TEST_F(ActivityTest, addEdgeFunctorTest) {
-    Activity a;
-    ActivityEdge e;
+    UmlManager m;
+    Activity a = m.create<Activity>();
+    ActivityEdge e = m.create<ActivityEdge>();
     a.getEdges().add(e);
-    ASSERT_TRUE(a.getEdges().size() == 1);
-    ASSERT_TRUE(&a.getEdges().front() == &e);
-    ASSERT_TRUE(a.getOwnedElements().size() == 1);
-    ASSERT_TRUE(&a.getOwnedElements().front() == &e);
-    ASSERT_TRUE(e.getActivity() == &a);
-    ASSERT_TRUE(e.getOwner() == &a);
+    ASSERT_EQ(a.getEdges().size(), 1);
+    ASSERT_EQ(a.getEdges().front(), e);
+    ASSERT_EQ(a.getOwnedElements().size(), 1);
+    ASSERT_EQ(a.getOwnedElements().front(), e);
+    ASSERT_TRUE(e.hasActivity());
+    ASSERT_TRUE(e.getActivity());
+    ASSERT_EQ(e.getActivityRef(), a);
+    ASSERT_EQ(e.getOwner(), &a);
 }
 
 TEST_F(ActivityTest, setEdgeActivityTest) {
-    Activity a;
-    ActivityEdge e;
+    UmlManager m;
+    Activity a = m.create<Activity>();
+    ActivityEdge e = m.create<ActivityEdge>();
     e.setActivity(&a);
-    ASSERT_TRUE(a.getEdges().size() == 1);
-    ASSERT_TRUE(&a.getEdges().front() == &e);
-    ASSERT_TRUE(a.getOwnedElements().size() == 1);
-    ASSERT_TRUE(&a.getOwnedElements().front() == &e);
-    ASSERT_TRUE(e.getActivity() == &a);
-    ASSERT_TRUE(e.getOwner() == &a);
+    ASSERT_EQ(a.getEdges().size(), 1);
+    ASSERT_EQ(a.getEdges().front(), e);
+    ASSERT_EQ(a.getOwnedElements().size(), 1);
+    ASSERT_EQ(a.getOwnedElements().front(), e);
+    ASSERT_TRUE(e.hasActivity());
+    ASSERT_TRUE(e.getActivity());
+    ASSERT_EQ(e.getActivityRef(), a);
+    ASSERT_EQ(e.getOwner(), &a);
 }

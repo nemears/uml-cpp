fc79bec701f70960b3c6e0492606e23932d80d7b
unknown <nickmears2@gmail.com>
2021-10-04 23:00:41 -0400

    added test for mounting comment

--- src/cpp/test/parsers/commentParserTest.cpp
+++ src/cpp/test/parsers/commentParserTest.cpp
@@ -43,3 +43,31 @@ TEST_F(CommentParserTest, commentEmitTest) {
     cout << generatedEmit << '\n';
     ASSERT_EQ(expectedEmit, generatedEmit);
 }
+
+TEST_F(CommentParserTest, mountAndEditCommentTest) {
+    UmlManager m;
+    Package& root = m.create<Package>();
+    Comment& comment = m.create<Comment>();
+    root.getOwnedComments().add(comment);
+    comment.setBody("foo bar");
+    m.setRoot(&root);
+    m.mount(ymlPath + "commentTests");
+
+    m.release(comment);
+    Comment& comment2 = root.getOwnedComments().front();
+    ASSERT_TRUE(comment2.hasOwningElement());
+    ASSERT_EQ(comment2.getOwningElementRef(), root);
+    ASSERT_TRUE(comment2.hasOwner());
+    ASSERT_EQ(comment2.getOwnerRef(), root);
+    ASSERT_TRUE(root.getOwnedComments().count(comment2.getID()));
+    ASSERT_TRUE(root.getOwnedComments().count(comment2.getID()));
+
+    ID commentID = comment2.getID();
+    m.release(comment2, root);
+    Comment& comment3 = m.aquire(commentID)->as<Comment>();
+    Package& root2 = comment3.getOwningElementRef().as<Package>();
+    ASSERT_TRUE(comment3.hasOwner());
+    ASSERT_EQ(comment3.getOwnerRef(), root2);
+    ASSERT_TRUE(root2.getOwnedComments().count(comment3.getID()));
+    ASSERT_TRUE(root2.getOwnedComments().count(comment3.getID()));
+}

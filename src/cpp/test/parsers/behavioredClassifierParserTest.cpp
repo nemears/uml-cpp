636d62e132f835392489f89b6553e0a6e2bf9afa
unknown <nickmears2@gmail.com>
2021-10-13 19:02:50 -0400

    expanded behavioredClassifier mount test

--- src/cpp/test/parsers/behavioredClassifierParserTest.cpp
+++ src/cpp/test/parsers/behavioredClassifierParserTest.cpp
@@ -3,6 +3,7 @@
 #include "test/yumlParsersTest.h"
 #include "uml/class.h"
 #include "uml/opaqueBehavior.h"
+#include "test/umlTestUtil.h"
 
 using namespace std;
 using namespace UML;
@@ -45,4 +46,46 @@ TEST_F(BehavioredClassifierParserTest, simpleClassEmitTest) {
     ASSERT_NO_THROW(generatedEmit = Parsers::emit(clazz));
     cout << generatedEmit << '\n';
     ASSERT_EQ(expectedEmit, generatedEmit);
+}
+
+TEST_F(BehavioredClassifierParserTest, mountFullBehavioredClassifierTest) {
+    UmlManager m;
+    Class& clazz = m.create<Class>();
+    OpaqueBehavior& classifierBehavior = m.create<OpaqueBehavior>();
+    clazz.setClassifierBehavior(classifierBehavior);
+    m.setRoot(&clazz);
+    m.mount(ymlPath + "behavioredClassifierTests");
+
+    ID clazzID = clazz.getID();
+    ID classifierBehaviorID = classifierBehavior.getID();
+    m.release(clazz);
+    ASSERT_FALSE(m.loaded(clazzID));
+    Class& clazz2 = m.aquire(clazzID)->as<Class>();
+    ASSERT_TRUE(clazz2.hasClassifierBehavior());
+    ASSERT_EQ(clazz2.getClassifierBehaviorRef(), classifierBehavior);
+    ASSERT_EQ(clazz2.getOwnedBehaviors().size(), 1);
+    ASSERT_EQ(clazz2.getOwnedBehaviors().front(), classifierBehavior);
+    ASSERT_NO_FATAL_FAILURE(ASSERT_RESTORED_NAMESPACE(classifierBehavior, clazz2));
+
+    m.release(classifierBehavior, clazz2);
+    ASSERT_FALSE(m.loaded(classifierBehaviorID));
+    ASSERT_FALSE(m.loaded(clazzID));
+    Class& clazz3 = m.aquire(clazzID)->as<Class>();
+    OpaqueBehavior& classifierBehavior2 = m.aquire(classifierBehaviorID)->as<OpaqueBehavior>();
+    ASSERT_TRUE(clazz3.hasClassifierBehavior());
+    ASSERT_EQ(clazz3.getClassifierBehaviorRef(), classifierBehavior2);
+    ASSERT_EQ(clazz3.getOwnedBehaviors().size(), 1);
+    ASSERT_EQ(clazz3.getOwnedBehaviors().front(), classifierBehavior2);
+    ASSERT_NO_FATAL_FAILURE(ASSERT_RESTORED_NAMESPACE(classifierBehavior2, clazz3));
+
+    m.release(clazz3, classifierBehavior2);
+    ASSERT_FALSE(m.loaded(classifierBehaviorID));
+    ASSERT_FALSE(m.loaded(clazzID));
+    OpaqueBehavior& classifierBehavior3 = m.aquire(classifierBehaviorID)->as<OpaqueBehavior>();
+    Class& clazz4 = m.aquire(clazzID)->as<Class>();
+    ASSERT_TRUE(clazz4.hasClassifierBehavior());
+    ASSERT_EQ(clazz4.getClassifierBehaviorRef(), classifierBehavior3);
+    ASSERT_EQ(clazz4.getOwnedBehaviors().size(), 1);
+    ASSERT_EQ(clazz4.getOwnedBehaviors().front(), classifierBehavior3);
+    ASSERT_NO_FATAL_FAILURE(ASSERT_RESTORED_NAMESPACE(classifierBehavior3, clazz4));
 }

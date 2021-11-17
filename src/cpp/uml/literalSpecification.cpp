2756dd6f252e1a8b0ada9c39af4950a7c2c02b13
nemears <nickmears2@gmail.com>
2021-11-17 16:38:29 -0500

    revived all literals

--- src/cpp/uml/literalSpecification.cpp
+++ src/cpp/uml/literalSpecification.cpp
@@ -4,10 +4,7 @@ using namespace UML;
 
 LiteralSpecification::LiteralSpecification() : Element(ElementType::LITERAL_SPECIFICATION) {}
 
-LiteralSpecification::LiteralSpecification(const LiteralSpecification& rhs) :
-ValueSpecification(rhs),
-NamedElement(rhs),
-Element(rhs, ElementType::LITERAL_SPECIFICATION) {}
+LiteralSpecification::LiteralSpecification(const LiteralSpecification& rhs) : Element(ElementType::LITERAL_SPECIFICATION) {}
 
 bool LiteralSpecification::isSubClassOf(ElementType eType) const {
     bool ret = ValueSpecification::isSubClassOf(eType);

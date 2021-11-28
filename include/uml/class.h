a6a939aeac10239353af557f2afffacffe106f07
nemears <nickmears2@gmail.com>
2021-11-28 16:29:41 -0500

    implemented class

--- include/uml/class.h
+++ include/uml/class.h
@@ -18,15 +18,15 @@ namespace UML{
 
         protected:
             OrderedSet<Property, Class> m_classOwnedAttrubutes = OrderedSet<Property, Class>(this);
-            // OrderedSet<Operation, Class> m_ownedOperations = OrderedSet<Operation, Class>(this);
-            // OrderedSet<Classifier, Class> m_nestedClassifiers = OrderedSet<Classifier, Class>(this);
+            OrderedSet<Operation, Class> m_ownedOperations = OrderedSet<Operation, Class>(this);
+            OrderedSet<Classifier, Class> m_nestedClassifiers = OrderedSet<Classifier, Class>(this);
             void referencingReleased(ID id) override;
             void referenceReindexed(ID oldID, ID newID) override;
             void restoreReferences() override;
             void referenceErased(ID id) override;
             Set<Property, Class>& getOwnedAttributesSet();
-            // Set<Operation, Class>& getOwnedOperationsSet();
-            // Set<Classifier, Class>& getNestedClassifiersSet();
+            Set<Operation, Class>& getOwnedOperationsSet();
+            Set<Classifier, Class>& getNestedClassifiersSet();
             void init();
             void copy(const Class& rhs);
             Class();
@@ -34,8 +34,8 @@ namespace UML{
             virtual ~Class();
             Class(const Class& clazz);
             OrderedSet<Property, Class>& getOwnedAttributes();
-            // OrderedSet<Operation, Class>& getOwnedOperations();
-            // OrderedSet<Classifier, Class>& getNestedClassifiers();
+            OrderedSet<Operation, Class>& getOwnedOperations();
+            OrderedSet<Classifier, Class>& getNestedClassifiers();
             bool isSubClassOf(ElementType eType) const override;
             static ElementType elementType() {
                 return ElementType::CLASS;
--- src/cpp/uml/class.cpp
+++ src/cpp/uml/class.cpp
@@ -9,16 +9,16 @@ void Class::referencingReleased(ID id) {
     StructuredClassifier::referencingReleased(id);
     m_classOwnedAttrubutes.release(id);
     // BehavioredClassifier::referencingReleased(id);
-    // m_ownedOperations.release(id);
-    // m_nestedClassifiers.release(id);
+    m_ownedOperations.release(id);
+    m_nestedClassifiers.release(id);
 }
 
 void Class::referenceReindexed(ID oldID, ID newID) {
     StructuredClassifier::referenceReindexed(oldID, newID);
     m_classOwnedAttrubutes.reindex(oldID, newID);
     // BehavioredClassifier::referenceReindexed(oldID, newID);
-    // m_ownedOperations.reindex(oldID, newID);
-    // m_nestedClassifiers.reindex(oldID, newID);
+    m_ownedOperations.reindex(oldID, newID);
+    m_nestedClassifiers.reindex(oldID, newID);
 }
 
 void Class::restoreReferences() {
@@ -32,38 +32,38 @@ void Class::referenceErased(ID id) {
     StructuredClassifier::referenceErased(id);
     m_classOwnedAttrubutes.eraseElement(id);
     // BehavioredClassifier::referenceErased(id);
-    // m_ownedOperations.eraseElement(id);
-    // m_nestedClassifiers.eraseElement(id);
+    m_ownedOperations.eraseElement(id);
+    m_nestedClassifiers.eraseElement(id);
 }
 
 Set<Property, Class>& Class::getOwnedAttributesSet() {
     return m_classOwnedAttrubutes;
 }
 
-// Set<Operation, Class>& Class::getOwnedOperationsSet() {
-//     return m_ownedOperations;
-// }
+Set<Operation, Class>& Class::getOwnedOperationsSet() {
+    return m_ownedOperations;
+}
 
-// Set<Classifier, Class>& Class::getNestedClassifiersSet() {
-//     return m_nestedClassifiers;
-// }
+Set<Classifier, Class>& Class::getNestedClassifiersSet() {
+    return m_nestedClassifiers;
+}
 
 void Class::init() {
     m_classOwnedAttrubutes.redefines(m_ownedAttributes);
     m_classOwnedAttrubutes.opposite(&Property::getClassSingleton);
     m_classOwnedAttrubutes.m_signature = &Class::getOwnedAttributesSet;
-    // m_ownedOperations.subsets(m_features);
-    // m_ownedOperations.subsets(m_ownedMembers);
-    // m_ownedOperations.opposite(&Operation::getClassSingleton);
-    // m_ownedOperations.m_signature = &Class::getOwnedOperationsSet;
-    // m_nestedClassifiers.subsets(m_ownedMembers);
-    // m_nestedClassifiers.m_signature = &Class::getNestedClassifiersSet;
+    m_ownedOperations.subsets(m_features);
+    m_ownedOperations.subsets(m_ownedMembers);
+    m_ownedOperations.opposite(&Operation::getClassSingleton);
+    m_ownedOperations.m_signature = &Class::getOwnedOperationsSet;
+    m_nestedClassifiers.subsets(m_ownedMembers);
+    m_nestedClassifiers.m_signature = &Class::getNestedClassifiersSet;
 }
 
 void Class::copy(const Class& rhs) {
     m_classOwnedAttrubutes = rhs.m_classOwnedAttrubutes;
-    // m_ownedOperations = rhs.m_ownedOperations;
-    // m_nestedClassifiers = rhs.m_nestedClassifiers;
+    m_ownedOperations = rhs.m_ownedOperations;
+    m_nestedClassifiers = rhs.m_nestedClassifiers;
 }
 
 Class::Class() : Element(ElementType::CLASS) {
@@ -89,13 +89,13 @@ OrderedSet<Property, Class>& Class::getOwnedAttributes() {
     return m_classOwnedAttrubutes;
 }
 
-// OrderedSet<Operation, Class>& Class::getOwnedOperations() {
-//     return m_ownedOperations;
-// }
+OrderedSet<Operation, Class>& Class::getOwnedOperations() {
+    return m_ownedOperations;
+}
 
-// OrderedSet<Classifier, Class>& Class::getNestedClassifiers() {
-//     return m_nestedClassifiers;
-// }
+OrderedSet<Classifier, Class>& Class::getNestedClassifiers() {
+    return m_nestedClassifiers;
+}
 
 bool Class::isSubClassOf(ElementType eType) const {
     bool ret = StructuredClassifier::isSubClassOf(eType);

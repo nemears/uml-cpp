32ddbac9095fde88947f235bdb1941f3d552378b
nemears <nickmears2@gmail.com>
2021-12-01 14:53:38 -0500

    started to bring back structuredClassifier with new sets

--- include/uml/structuredClassifier.h
+++ include/uml/structuredClassifier.h
@@ -9,41 +9,9 @@ namespace UML {
 
     class StructuredClassifier : virtual public Classifier {
         protected:
+            Set<ConnectableElement, StructuredClassifier> m_roles = Set<ConnectableElement, StructuredClassifier>(this);
             Set<Property, StructuredClassifier> m_ownedAttributes = Set<Property, StructuredClassifier>(this);
-            // Sequence<ConnectableElement> m_role = Sequence<ConnectableElement>(this);
-            // Sequence<Property> m_parts = Sequence<Property>(this);
-            // class AddOwnedAttributeFunctor : public TemplateAbstractSequenceFunctor<Property,StructuredClassifier> {
-            //     public:
-            //         AddOwnedAttributeFunctor(StructuredClassifier* me) : TemplateAbstractSequenceFunctor(me) {};
-            //         void operator()(Property& el) const override;
-            //         void operator()(ID id) const override;
-            // };
-            // class RemoveOwnedAttributeFunctor : public TemplateAbstractSequenceFunctor<Property,StructuredClassifier> {
-            //     public:
-            //         RemoveOwnedAttributeFunctor(StructuredClassifier* me) : TemplateAbstractSequenceFunctor(me) {};
-            //         void operator()(Property& el) const override;
-            // };
-            // class AddRoleFunctor : public TemplateAbstractSequenceFunctor<ConnectableElement,StructuredClassifier> {
-            //     public:
-            //         AddRoleFunctor(StructuredClassifier* me) : TemplateAbstractSequenceFunctor(me) {};
-            //         void operator()(ConnectableElement& el) const override;
-            //         void operator()(ID id) const override;
-            // };
-            // class RemoveRoleFunctor : public TemplateAbstractSequenceFunctor<ConnectableElement,StructuredClassifier> {
-            //     public:
-            //         RemoveRoleFunctor(StructuredClassifier* me) : TemplateAbstractSequenceFunctor(me) {};
-            //         void operator()(ConnectableElement& el) const override;
-            // };
-            // class AddPartFunctor : public TemplateAbstractSequenceFunctor<Property, StructuredClassifier> {
-            //     public:
-            //         AddPartFunctor(StructuredClassifier* me) : TemplateAbstractSequenceFunctor(me) {};
-            //         void operator()(Property& el) const override;
-            // };
-            // class RemovePartFunctor : public TemplateAbstractSequenceFunctor<Property, StructuredClassifier> {
-            //     public:
-            //         RemovePartFunctor(StructuredClassifier* me) : TemplateAbstractSequenceFunctor(me) {};
-            //         void operator()(Property& el) const override;
-            // };
+            Set<Property, StructuredClassifier> m_parts = Set<Property, StructuredClassifier>(this);
             void referencingReleased(ID id) override;
             void referenceReindexed(ID oldID, ID newID);
             void restoreReferences() override;
@@ -55,8 +23,8 @@ namespace UML {
             StructuredClassifier(const StructuredClassifier& clazz);
             virtual ~StructuredClassifier();
             Set<Property, StructuredClassifier>& getOwnedAttributes();
-            // Sequence<ConnectableElement>& getRole();
-            // Sequence<Property>& getParts();
+            Set<ConnectableElement, StructuredClassifier>& getRoles();
+            Set<Property, StructuredClassifier>& getParts();
             bool isSubClassOf(ElementType eType) const override;
             static ElementType elementType() {
                 return ElementType::STRUCTURED_CLASSIFIER;
--- src/cpp/uml/structuredClassifier.cpp
+++ src/cpp/uml/structuredClassifier.cpp
@@ -7,15 +7,15 @@ using namespace UML;
 void StructuredClassifier::referencingReleased(ID id) {
     Classifier::referencingReleased(id);
     m_ownedAttributes.release(id);
-    // m_role.elementReleased(id, &StructuredClassifier::getRole);
-    // m_parts.elementReleased(id, &StructuredClassifier::getParts);
+    m_roles.release(id);
+    m_parts.release(id);
 }
 
 void StructuredClassifier::referenceReindexed(ID oldID, ID newID) {
     Classifier::referenceReindexed(oldID, newID);
     m_ownedAttributes.reindex(oldID, newID);
-    // m_role.reindex(oldID, newID, &StructuredClassifier::getRole);
-    // m_parts.reindex(oldID, newID, &StructuredClassifier::getParts);
+    m_roles.reindex(oldID, newID);
+    m_parts.reindex(oldID, newID);
 }
 
 void StructuredClassifier::restoreReferences() {
@@ -36,18 +36,26 @@ void StructuredClassifier::restoreReferences() {
 void StructuredClassifier::referenceErased(ID id) {
     Classifier::referenceErased(id);
     m_ownedAttributes.eraseElement(id);
-    // m_role.elementErased(id);
-    // m_parts.elementErased(id);
+    m_roles.eraseElement(id);
+    m_parts.eraseElement(id);
 }
 
 void StructuredClassifier::init() {
+    m_roles.subsets(m_members);
+    m_roles.m_signature = &StructuredClassifier::getRoles;
     m_ownedAttributes.subsets(m_attributes);
+    m_ownedAttributes.subsets(m_roles);
     m_ownedAttributes.subsets(m_ownedMembers);
     m_ownedAttributes.m_signature = &StructuredClassifier::getOwnedAttributes;
+    m_parts.subsets(m_ownedAttributes);
+    m_parts.m_signature = &StructuredClassifier::getParts;
+    m_parts.m_readOnly = true;
 }
 
 void StructuredClassifier::copy(const StructuredClassifier& rhs) {
+    m_roles = rhs.m_roles;
     m_ownedAttributes = rhs.m_ownedAttributes;
+    m_parts = rhs.m_parts;
 }
 
 StructuredClassifier::StructuredClassifier() : Element(ElementType::STRUCTURED_CLASSIFIER) {
@@ -66,13 +74,13 @@ Set<Property, StructuredClassifier>& StructuredClassifier::getOwnedAttributes() 
     return m_ownedAttributes;
 }
 
-// Sequence<ConnectableElement>& StructuredClassifier::getRole() {
-//     return m_role;
-// }
+Set<ConnectableElement, StructuredClassifier>& StructuredClassifier::getRoles() {
+    return m_roles;
+}
 
-// Sequence<Property>& StructuredClassifier::getParts() {
-//     return m_parts;
-// }
+Set<Property, StructuredClassifier>& StructuredClassifier::getParts() {
+    return m_parts;
+}
 
 bool StructuredClassifier::isSubClassOf(ElementType eType) const {
     bool ret = Classifier::isSubClassOf(eType);

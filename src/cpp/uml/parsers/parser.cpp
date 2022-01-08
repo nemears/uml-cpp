29b864b05363cff047b9be391a4a9b3ad101f6ba
nemears <nickmears2@gmail.com>
2022-01-08 08:58:57 -0500

    fixed parseAndAddToSequence to work with new sets when parsing with individual strategy

--- src/cpp/uml/parsers/parser.cpp
+++ src/cpp/uml/parsers/parser.cpp
@@ -289,11 +289,11 @@ template <class T = Element, class U = Element, class S = Set<T,U>> void parseAn
         std::string idStr = path.substr(path.find_last_of("/") + 1, path.find_last_of("/") + 29);
         if (isValidID(idStr)) {
             ID id = ID::fromString(idStr);
-            if (data.m_manager->UmlManager::loaded(id)) {
-                (el.*signature)().add(data.m_manager->get<T>(id)); // Too slow? makes it easier
-            } else {
+            // if (data.m_manager->UmlManager::loaded(id)) {
+            //     (el.*signature)().add(data.m_manager->get<T>(id)); // Too slow? makes it easier
+            // } else {
                 (el.*signature)().add(id);
-            }
+            // }
         } else {
             throw UmlParserException("Invalid id for path, was the data specified as individual, that can only work on a mount!", data.m_path.string(), node);
         }

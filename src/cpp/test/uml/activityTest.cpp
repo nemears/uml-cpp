4d5528316f5314543c557b59c1496bf5f0a7f49e
unknown <nickmears2@gmail.com>
2022-03-15 10:15:26 -0400

    finished reimplementing node and activity tests

--- src/cpp/test/uml/activityTest.cpp
+++ src/cpp/test/uml/activityTest.cpp
@@ -1,7 +1,5 @@
 #include "gtest/gtest.h"
-#include "uml/activity.h"
-#include "uml/activityNode.h"
-#include "uml/activityEdge.h"
+#include "uml/uml-stable.h"
 
 using namespace UML;
 
@@ -11,60 +9,56 @@ class ActivityTest : public ::testing::Test {
 
 TEST_F(ActivityTest, addNodeFunctorTest) {
     UmlManager m;
-    Activity a = m.create<Activity>();
-    ActivityNode n = m.create<ActivityNode>();
+    Activity& a = *m.create<Activity>();
+    ActivityNode& n = *m.create<ActivityNode>();
     a.getNodes().add(n);
     ASSERT_EQ(a.getNodes().size(), 1);
     ASSERT_EQ(a.getNodes().front(), n);
     ASSERT_EQ(a.getOwnedElements().size(), 1);
     ASSERT_EQ(*a.getOwnedElements().begin(), n);
-    ASSERT_TRUE(n.hasActivity());
     ASSERT_TRUE(n.getActivity());
-    ASSERT_EQ(n.getActivityRef(), a);
+    ASSERT_EQ(*n.getActivity(), a);
     ASSERT_EQ(*n.getOwner(), a);
 }
 
 TEST_F(ActivityTest, setNodeActivityTest) {
     UmlManager m;
-    Activity a = m.create<Activity>();
-    ActivityNode n = m.create<ActivityNode>();
+    Activity& a = *m.create<Activity>();
+    ActivityNode& n = *m.create<ActivityNode>();
     n.setActivity(a);
     ASSERT_EQ(a.getNodes().size(), 1);
     ASSERT_EQ(a.getNodes().front(), n);
     ASSERT_EQ(a.getOwnedElements().size(), 1);
     ASSERT_EQ(*a.getOwnedElements().begin(), n);
-    ASSERT_TRUE(n.hasActivity());
     ASSERT_TRUE(n.getActivity());
-    ASSERT_EQ(n.getActivityRef(), a);
+    ASSERT_EQ(*n.getActivity(), a);
     ASSERT_EQ(*n.getOwner(), a);
 }
 
 TEST_F(ActivityTest, addEdgeFunctorTest) {
     UmlManager m;
-    Activity a = m.create<Activity>();
-    ActivityEdge e = m.create<ActivityEdge>();
+    Activity& a = *m.create<Activity>();
+    ActivityEdge& e = *m.create<ActivityEdge>();
     a.getEdges().add(e);
     ASSERT_EQ(a.getEdges().size(), 1);
     ASSERT_EQ(a.getEdges().front(), e);
     ASSERT_EQ(a.getOwnedElements().size(), 1);
     ASSERT_EQ(*a.getOwnedElements().begin(), e);
-    ASSERT_TRUE(e.hasActivity());
     ASSERT_TRUE(e.getActivity());
-    ASSERT_EQ(e.getActivityRef(), a);
+    ASSERT_EQ(*e.getActivity(), a);
     ASSERT_EQ(*e.getOwner(), a);
 }
 
 TEST_F(ActivityTest, setEdgeActivityTest) {
     UmlManager m;
-    Activity a = m.create<Activity>();
-    ActivityEdge e = m.create<ActivityEdge>();
+    Activity& a = *m.create<Activity>();
+    ActivityEdge& e = *m.create<ActivityEdge>();
     e.setActivity(&a);
     ASSERT_EQ(a.getEdges().size(), 1);
     ASSERT_EQ(a.getEdges().front(), e);
     ASSERT_EQ(a.getOwnedElements().size(), 1);
     ASSERT_EQ(*a.getOwnedElements().begin(), e);
-    ASSERT_TRUE(e.hasActivity());
     ASSERT_TRUE(e.getActivity());
-    ASSERT_EQ(e.getActivityRef(), a);
+    ASSERT_EQ(*e.getActivity(), a);
     ASSERT_EQ(*e.getOwner(), a);
 }

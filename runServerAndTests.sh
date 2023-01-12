9a6096557d8c45a74a417f87adf862ed8539ada3
nemears <nickmears2@gmail.com>
2023-01-11 22:52:36 -0500

    added some options to the bash script for testing/pipeline

--- runServerAndTests.sh
+++ runServerAndTests.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-
+outputToNull=false
+while getopts nf: flag
+do
+    case "${flag}" in
+        n) outputToNull=true;;
+        f) filter=${OPTARG};;
+    esac
+done
+if [ -n "$filter" ] ; then
+    testArgs=--gtest_filter=$filter
+fi
 cd build
-./uml-server &
+if $outputToNull ; then
+    ./uml-server > /dev/null &
+else
+    ./uml-server &
+fi
 server_id=$!
-./umltest
+./umltest $testArgs
 test_result=$?
 kill $server_id
 exit $test_result

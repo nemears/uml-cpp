63e773873f1c153d398c0c61055a8b107d361e09
nemears <nickmears2@gmail.com>
2024-07-24 13:57:05 -0400

    added function to make uml-cpp derivations, made an older version to pin for other programs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,36 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
+        let 
+          pkgs = nixpkgs.legacyPackages.${system};
+          
+          # function to create uml-cpp derivation from certain source
+          buildUmlCpp = { src , version } : pkgs.stdenv.mkDerivation {
+            inherit src;
+            name = "uml-cpp";
+            nativeBuildInputs = with pkgs; [pkg-config];
+            buildInputs = with pkgs; [cmake pkg-config clang gnumake coreutils yaml-cpp];
+            cmakeFlags = [
+              "-DUML_BUILD_TESTS=OFF"
+            ];
+            installPhase = 
+            ''
+              mkdir -p $out/lib/pkgconfig $out/include
+              cp -r libuml.so $out/lib
+              cp -r $src/include/uml $out/include
+              touch $out/lib/pkgconfig/uml-cpp.pc
+              echo "prefix=$out
+
+              Name: uml
+              Description: uml modeling C++ api
+              Version: ${version}
+              Libs: -L$out/lib -luml
+              Cflags: -I$out/include" > $out/lib/pkgconfig/uml-cpp.pc
+
+              export PKG_CONFIG_PATH=PKG_CONFIG_PATH:$out/lib/pkgconfig
+            '';
+          };
+        in
         {
           # dev shell for nix develop (good for commandline dev)
           devShells.default = pkgs.mkShell {
@@ -31,30 +60,18 @@
             ];
           };
 
-          packages.uml-cpp = pkgs.stdenv.mkDerivation {
-            name = "uml-cpp";
+          packages.uml-cpp = buildUmlCpp {
             src = ./.;
-            nativeBuildInputs = with pkgs; [pkg-config];
-            buildInputs = with pkgs; [cmake pkg-config clang gnumake coreutils yaml-cpp];
-            cmakeFlags = [
-              "-DUML_BUILD_TESTS=OFF"
-            ];
-            installPhase = 
-            ''
-              mkdir -p $out/lib/pkgconfig $out/include
-              cp -r libuml.so $out/lib
-              cp -r $src/include/uml $out/include
-              touch $out/lib/pkgconfig/uml-cpp.pc
-              echo "prefix=$out
-
-              Name: uml
-              Description: uml modeling C++ api
-              Version: 0.4.0
-              Libs: -L$out/lib -luml
-              Cflags: -I$out/include" > $out/lib/pkgconfig/uml-cpp.pc
-
-              export PKG_CONFIG_PATH=PKG_CONFIG_PATH:$out/lib/pkgconfig
-            '';
+            version = "0.4.0";
+          };
+          packages.uml-cpp_0_3_6 = buildUmlCpp {
+            src = pkgs.fetchFromGitHub {
+              owner = "nemears";
+              repo = "uml-cpp";
+              rev = "v0.3.6";
+              hash = "sha256-crZCrVuUpW217dQ4WuN8qAcVtbMZSsbKhhHnnC8OEDc=";
+            };
+            version = "0.3.6";
           };
 
           packages.default = self.packages.${system}.uml-cpp;
